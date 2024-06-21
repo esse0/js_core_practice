b646cd1f0476b1ddc01eb17a72f22ab064d77265
esse <redstrixw@gmail.com>
2024-06-21 21:59:30 +0300

    added 2 task

---
+++ [2] Variables var, let, const/VariableScope_1.js
@@ -0,0 +1,11 @@
+function funcScope(){
+    const constVariable = "Const variable";
+    let letVariable = "Let variable";
+    var varVariable = "Var variable";
+}
+
+console.log(varVariable);
+console.log(letVariable);
+console.log(constVariable);
+
+// Все переменные ограничены областью видимости функции
